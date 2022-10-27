bad5516e604ced344450978b10cd1d82eb87edc7
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-27 08:02:11 +0530

    todo app using react js

--- src/App.js
+++ src/App.js
@@ -13,26 +13,27 @@ function App() {
     getLocalTodos();
   }, [])
   useEffect(()=>{
+    const filterHandler = () => {
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    }
+    const saveLocalTodos = ()=> {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }
     filterHandler();
     saveLocalTodos();
   },[todos, status])
 
-  const filterHandler = () => {
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true))
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false))
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
-    }
-  }
-  const saveLocalTodos = ()=> {
-      localStorage.setItem("todos", JSON.stringify(todos))
-  }
+  
   const getLocalTodos = () => {
     if(localStorage.getItem('todos')=== null){
       localStorage.setItem('todos', JSON.stringify([]))
